e5f924483fabbfaefc0edcb599c4ef3de12647be
Jason Barto <jason.p.barto@gmail.com>
2017-11-06 23:09:01 +0000

    initial commit of working dot commands

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dot Commands
+
+A collection of docker build files to encapsulate common commands and environments.  Made accessible by shell alias.  This project maintains both the build files for different container images as well as the alias definitions.  A script to install the aliases may also soon be included.
---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "alias _docker_run='docker run -ti --rm -v $HOME/.vimrc:/root/.vimrc'" > alias
+echo "alias _docker_run_aws='_docker_run -v $HOME/.aws:/root/.aws'" >> alias
+
+for project in `ls src`
+do
+    docker build -t "jpbarto/${project}:dotcommand" "src/${project}"
+    ALIAS_CMD=`grep '# DOTCOMMAND_ALIAS ' "src/${project}/Dockerfile" | sed -e 's/# DOTCOMMAND_ALIAS //'`
+    echo "alias ${project}='${ALIAS_CMD} jpbarto/${project}:dotcommand'" >> alias
+done
